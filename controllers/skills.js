ea2131a5f2b94c38de5bedbf63d373d65949fddc
Michael Crawford <webdevcrawford@gmail.com>
2022-06-15 21:03:20 -0600

    adds skills controller functions

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,9 +1,39 @@
+import { Skills } from '../models/skills.js'
+
 function index(req, res) {
-  res.render('skills', {
-    title: "Skills"
+  Skills.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      title: "Skills List",
+      skills: skills
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+function newSkill(req, res) {
+  res.render('skills/new', {
+    title: "Add a new skill",
+  })
+}
+
+function create(req, res) {
+  Skills.create(req.body)
+  .then(skill => {
+    console.log(skill)
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
   })
 }
 
 export {
-  index
+  index,
+  newSkill as new,
+  create
 }
